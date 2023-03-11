a9be1460b84da24e5d6321e412e56cda355df813
김재령 <wofud0321@naver.com>
2023-03-12 00:18:25 +0900

    회원 도메인 개발 2
    테스트케이스 작성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)  // 스프링과 같이 실행한다는 의미
+@SpringBootTest // 스프링 부트를 띄운 상태로 실행
+@Transactional  // 테스트가 끝나면 롤백해준다.
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test()
+    public void 중복_회원_예외() throws Exception{
+        assertThrows(IllegalStateException.class, () -> {
+            // given
+            Member member1 = new Member();
+            member1.setName("kim1");
+
+            Member member2 = new Member();
+            member2.setName("kim1");
+
+            // when
+            memberService.join(member1);
+            memberService.join(member2);
+
+
+            // then
+            fail("예외가 발생해야 한다.");
+        });
+    }
+
+}
