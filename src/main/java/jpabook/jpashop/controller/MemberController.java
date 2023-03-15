58711a5c53fec121cccf17da403f188fc243aa63
김재령 <wofud0321@naver.com>
2023-03-15 13:57:30 +0900

    상품 등록까지

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -40,4 +42,10 @@ public class MemberController {
         return "redirect:/";    // 첫번째 페이지로 넘어간다
     }
 
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
