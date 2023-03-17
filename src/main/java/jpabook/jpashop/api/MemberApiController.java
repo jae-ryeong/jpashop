7db49446d12eed774e412ca373e91160c711e665
김재령 <wofud0321@naver.com>
2023-03-17 22:09:53 +0900

    JPA2 - API
    조회용 샘플 데이터 입력

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,21 +3,48 @@ package jpabook.jpashop.api;
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // @Controller + @ResponseBody
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result MemberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDTO> collect = findMembers.stream()
+                .map(m -> new MemberDTO(m.getName()))   // member Entity에서 이름을 꺼내와서 DTO로 변환
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+    @Data @AllArgsConstructor
+    static class MemberDTO{
+        private String name;
+    }
+
     @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {   // 이 방식은 사용하지 않는다.
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
@@ -41,6 +68,25 @@ public class MemberApiController {
     }
     @Data
     static class CreateMemberRequest {
+        private String name;
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, // update용 응답 DTO
+                                               @RequestBody @Valid UpdateMemberRequest request) {   // update용 요청 DTO
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());   // @AllArgsConstructor을 사용했기 때문에 id와 name 둘다 넘길수 있는 생성자가 필요하다
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
         private String name;
     }
 }
