f1974dd802e6b5439852fd609f6407d5e2661e9f
김재령 <wofud0321@naver.com>
2023-03-21 13:56:20 +0900

    V5, V6

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -23,6 +25,36 @@ public class OrderQueryRepository { // V4에 알맞은 쿼리를 위한 레포�
         return result;
     }
 
+    public List<OrderQueryDto> findAllByDto_optimization() {    // 쿼리를 한번날리고 메모리에서 값을 가져와 세팅해준다.
+        List<OrderQueryDto> result = findOrders();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class  // in으로 orderIds에 있는 Id 2개가 한번에 쿼리문을 처리해준다.
+        ).setParameter("orderIds", orderIds).getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));   // groupingBy로 Map으로 바꿔준다. Key값은 orderId이다.
+        return orderItemMap;
+    }
+
+    private static List<Long> toOrderIds(List<OrderQueryDto> result) {
+        List<Long> orderIds = result.stream()   // orderIds에 result에서 뽑아온 OrderId 2개가 List형태로 모두 담긴다
+                        .map(o -> o.getOrderId())
+                        .collect(Collectors.toList());
+        return orderIds;
+    }
+
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {  // 이 메서드가 호출될때, 두번째와 세번째 쿼리가 발생한다.
         return em.createQuery(
                 "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
@@ -32,7 +64,7 @@ public class OrderQueryRepository { // V4에 알맞은 쿼리를 위한 레포�
         ).setParameter("orderId", orderId).getResultList();
     }
 
-    private List<OrderQueryDto> findOrders() {  // 첫번째 쿼리, fetch join이 아니다.
+    private List<OrderQueryDto> findOrders() {  // V4와 V5의 첫번째 쿼리, fetch join이 아니다.
         return em.createQuery(
                         "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
                                 " from Order o" +
@@ -40,4 +72,18 @@ public class OrderQueryRepository { // V4에 알맞은 쿼리를 위한 레포�
                                 " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
     };
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new " +
+                        " jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class   // 쿼리문 다음 매개변수가 반환타입이다.
+        ).getResultList();
+    }
+
+
 }
